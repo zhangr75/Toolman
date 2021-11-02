2a26047c56bfb14b0e1e294afe2809187cf5d77d
Blablabla-Jin <4299jin@gmail.com>
2021-11-02 18:38:05 -0400

    fixed some missing thinghs

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,4 @@
 function initMap() {
-                var location= { lat: 43.24061163021694, lng: -79.86368627437132}
-                var map = new google.maps.Map(document.getElementById("map"),{zoom: 4, center: location});
+                const location= { lat: 43.24061163021694, lng: -79.86368627437132}
+                const map = new google.maps.Map(document.getElementById("map"),{zoom: 12, center: location});
                 }
