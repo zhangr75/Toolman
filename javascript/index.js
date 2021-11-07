79b222426b80d6f0d94cc3de3186ad71913fe864
Blablabla-Jin <4299jin@gmail.com>
2021-11-07 02:27:10 -0500

    validation by js

--- javascript/index.js
+++ javascript/index.js
@@ -1,33 +1,118 @@
-var map, marker;
+var map, marker;  //variables to set up map and markers
 
+
+//Initialize the Google Map API
 function initMap() {
-                map = new google.maps.Map(document.getElementById("map"),{
-                    zoom: 13, 
-                    center: {lat:43.24458199937876,  lng: -79.87154251830432}});
+    //set up map object by initializing the zoom index and the position: lat, lag
+    map = new google.maps.Map(document.getElementById("map"),{
+        zoom: 13, 
+        center: {lat:43.24458199937876,  lng: -79.87154251830432}});
 
-                var infoWindow = new google.maps.InfoWindow;
-                marker = new google.maps.Marker({
-                    map: map,
-                });
-            }
+        var infoWindow = new google.maps.InfoWindow;
+
+        //set up marker object
+        marker = new google.maps.Marker({
+            map: map,
+        });
+}
 
     
-
+//HTML5 Geolocation API
 function showposition(){
-        if(navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(function(position) {
-                    var currentpositon = {
-                      lat: position.coords.latitude,
-                      lng: position.coords.longitude
-                    };
-                    marker.setPosition(currentpositon);
-                    map.setCenter(currentpositon, 12.5)
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var currentpositon = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            marker.setPosition(currentpositon);//set the location of marker
+            map.setCenter(currentpositon, 12.5)//reset the centre of the map
+        });
+    }
+    else{
+        alert("Ops, your browser does not support HTML5 geolocation.");
+    }
+}
 
-                });
-            }else{
-            alert("Ops, your browser does not support HTML5 geolocation.");
-        }
-  }
+//Check the validation of the input this is the aggregate function
+function validationForSignUp(form){
+
+    var numVal = validPhoneNumber(form);
+    var emailVal = validEmail(form);
+    var pwVal = validPW(form);
+    if (numVal == false || emailVal == false || pwVal == false){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+function validationForLogIn(form){
+
+    var emailVal = validEmail(form);
+    var pwVal = validPW(form);
+    if (emailVal == false || pwVal == false){
+        return false;
+    }
+    else{
+        return true;
+    }
+
+}
+
+
+function validPhoneNumber(form){
+
+    if(form.PhoneNumber.value == ""){
+        alert("Need to fill up Phone Number")
+        return false;
+    }
+
+    //Regular expression to restrict the form that user entering the phone number
+    else if (!(/^(\+1)?[\s\-]?\(?[0-9]{3}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{4}$/.test(form.PhoneNumber.value))){
+        alert("Invalid form of Phone Number");
+        return false;
+    }
+    return true;
+
+    //found a more specific way to set the pattern which prevent some inputs like (905)+1344355 passing the validation
+    /*
+    var standard = form.PhoneNumber.value.replace(/^(\+1)/g, "");
+    var text = standard.replace(/[\(\)\s\-]/g, "");
+    if (form.PhoneNumber.value == ""){
+        alert("Need to fill something")
+        return false;
+    }
+    else if (/^[0-9]{10}$/.test(text)){
+        alert("Invalid form of phone number");
+        return false;
+    }
+    */
+
+}
+
+function validEmail(form){
+    if(form.EMailAddress.value == ""){
+        alert("Need to fill up E-Mail Address.")
+        return false;
+    }
+
+    //Regular expression to restrict the form that user entering the email address
+    else if (!(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,})+$/.test(form.EMailAddress.value))){
+        alert("Invalid form of E-mail Address");
+        return false;
+    }
+    return true;
+}
+
+function validPW(form){
+    if(form.Userpw.value == ""){
+        alert("Need to fill up Password.")
+        return false;
+    }
+    return true;
+}
 
 
 
