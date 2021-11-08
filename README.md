88f81e7e36845f8852b8b7d8491c181dcb2e2f9b
Run Zhang <zhangr75@mcmaster.ca>
2021-11-07 20:34:30 -0500

    animation added

--- README.md
+++ README.md
@@ -5,29 +5,12 @@
 •Member 2: Boming Jin (jinb5)
 •Live Server: http://3.13.254.133/
 
-We did all add-on tasks
+We did add-on tasks.
 
-Answer for HTML picture and source tag
-i). <picture>
-        <source media="(min-width:450px)" srcset="images/loconmap.jpg" >
-              <img src="images/smallloconmap.jpg" alt="map" style = "width: auto;" >
-    </picture>
+· Click search button to show search result on the left side and the marker on right side. Click result or more info link in markr will go the individualobjectpage.
 
-The selector will show locomap.jpg at first, and as the screen narrows, the width of the 
-image will start to get shorter. When the width is less than 450px, the selector will show 
-samlllocomap.jpg which is the small picture.
+· Click gps icon to locate current user position.
 
-ii).
-Three positive goals:
-1.Suppose you use the img tag for high-resolution images. In this case, the same image will 
-be applied on every device that has the application running and will affect the performance 
-of lower resolution devices (e.g., mobile devices).
-2.In most cases, we need to handle resolution switching and artwork at the same time, then the 
-picture tag is the best choice.
-3.Img tags result in longer image loading times and top-to-bottom image loading piece by piece.
-4.We could store images in a landscape orientation or portrait orientation for future needs. Which makes
-the site looks good.
+· Add buton located in left bottom of the page, click to submission page.
 
-iii).
-When we are using <picture> and <source> tags, we need to store more pictures than we showed on the site,
-so that will waste space to store those images. Because of that, we could use fewer pictures or using links to get the image.
+· Right click on the map in submission page will auto fill the coordinates and show marker.
--- javascript/index.js
+++ javascript/index.js
@@ -27,6 +27,8 @@ function showposition(){
             };
             marker.setPosition(currentpositon);//set the location of marker
             map.setCenter(currentpositon, 12.5)//reset the centre of the map
+            marker.setAnimation(google.maps.Animation.DROP);
+            marker.setAnimation(google.maps.Animation.BOUNCE);
         });
     }
     else{
@@ -53,7 +55,16 @@ function showresult(){
         infoWindow.setContent(searchexample);
         infoWindow.open(map, marker);
         });
+    marker.setAnimation(google.maps.Animation.DROP);
+    marker.addListener("mouseover", bounceanimation);
+}
 
+function bounceanimation() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
 }
 
 //Check the validation of the input this is the aggregate function
@@ -135,103 +146,4 @@ function validPW(form){
     }
     return true;
 }
-
-
-
-
-
-                /**var bounds = new google.maps.LatLngBounds();
-
-                
-                var location= [
-                /**{
-                    "title": 'Coco MilkTea',
-                    "lat": '43.2543927046955',
-                    "lng": '-79.86604390141395',
-                    "description": 'asdafwrfqwetfwe'
-                    "address": ''
-                }
-                ['Coco MilkTea', 43.2543927046955, -79.86604390141395],
-                ['The Ship', 43.25221779075225, -79.8699729075916]
-                ];
-
-
-                var infoWindowContent = [
-                ['<div class="info_content">' +
-                '<h3>Coco MilkTea</h3>' +
-                '<p>Address: 96 Main St E, Hamilton, ON L8N 1G3</p>' + '</div>'+
-                '<a href = "individual_sample.html"><u>More info</u></a>' +
-                '</div>'],
-                ['<div class="info_content">' +
-                '<h3>The Ship</h3>' +
-                '<p>Address: 23 Augusta St, Hamilton, ON L8N 1P2</p>' +
-                '</div>'+
-                '<a href = "individual_sample.html"><u>More info</u></a>' +
-                '</div>']
-                ];
-
-                var infoWindow = new google.maps.InfoWindow(), maker, i;
-
-                for( i = 0; i < location.length; i++ ) {
-                    var position = new google.maps.LatLng(location[i][1], location[i][2]);
-                    bounds.extend(position);
-                    marker = new google.maps.Marker({
-                        position: position,
-                        map: map,
-                        title: location[i][0]
-                    });
-                
-                    // Add info window to marker    
-                    google.maps.event.addListener(marker, 'click', (function(marker, i) {
-                        return function() {
-                            infoWindow.setContent(infoWindowContent[i][0]);
-                            infoWindow.open(map, marker);
-                        }
-                    })(marker, i));
-
-                    /**
-                    google.maps.event.addListener(marker, 'mouseout', function(){
-                        infoWindow.close();
-                    });
-                    *
-
-                    // Center the map
-                    map.fitBounds(bounds);
-                    
-                    // Load initialize function
-                    google.maps.event.addDomListener(window, 'load', initMap);
-                }**/
-                
-                
-
-/**function getLocation(){
-
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(userLoc);
-        
-    }
-    else{
-        document.getElementById("userLocation").innerHTML = "Failed to get the location.";
-    }
-
-}
-
-function userLoc(position){
-
-    userLocation[0] = 'userLocation';
-    userLocation[1] = position.coords.latitude;
-    userLocation[2] = position.coords.longitude;
-
-}
-
-function showLocation(){
-
-    if (!userLocation.length){
-        document.getElementById("userLocation").innerHTML = "Don't know the location right now";
-    }
-    else{
-        document.getElementById("userLocation").innerHTML = userLocation;
-
-    }
-}**/
 
