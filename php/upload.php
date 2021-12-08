9cff6ba041a286487c9e945aa43d1b5a6c19103d
Blablabla-Jin <4299jin@gmail.com>
2021-12-08 02:19:33 -0500

    upload file things

--- php/upload.php
+++ php/upload.php
@@ -3,31 +3,37 @@
     use Aws\S3\S3Client;
     require './../vendor/autoload.php';
     
-    var_dump($_FILES['fileToUpload']);
+    
 	if(isset($_FILES['fileToUpload'])){
 		$file_name = $_FILES['fileToUpload']['name'];   
 		$temp_file_location = $_FILES['fileToUpload']['tmp_name']; 
-        var_dump($file_name);
-        var_dump($temp_file_location);
+        
+        //var_dump($_FILES['fileToUpload']);
+        //var_dump($file_name);
+        //var_dump($temp_file_location);
 	    
 		
 
-		$s3 = new S3Client([
-			'region'  => 'ca-central-1',
-			'version' => 'latest',
-			'credentials' => [
-				'key'    => "",
-				'secret' => "",
-			]
-		]);		
+		// $s3 = S3Client::factory([
+		// 	'region'  => 'ca-central-1',
+		// 	'version' => 'latest',
+		// 	'credentials' => [
+		// 		'key'    => "AKIA5LWSVAHKAM7T7DHW",
+		// 		'secret' => "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",
+		// 	]
+		// ]);		
 
-		$result = $s3->putObject([
-			'Bucket' => 'toolman',
-			'Key'    => $file_name,
-			'SourceFile' => $temp_file_location			
-		]);
-
-        var_dump($result);
+		// $result = $s3->putObject([
+		// 	'Bucket' => 'toolman',
+		// 	'Key'    => $file_name,
+		// 	'SourceFile' => $temp_file_location			
+		// ]);
+        $imageUrl = 'https://toolman.s3.ca-central-1.amazonaws.com/' . $file_name;
+        echo $file_name;
+        echo "<br>" . $temp_file_location;
+        echo "<br>" . $imageUrl . "<br>";
+        var_dump($_FILES['fileToUpload']);
+        //var_dump($result);
         
 	}
 
